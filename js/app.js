38821f4e2a9305f3d44ad55618ef6fd0763068b6
Stephen Corry <stevemcorry@gmail.com>
2016-11-11 12:20:22 -0700

    fix Isolate Scope

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ var App = angular.module('assessment',['ui.router']);
 App.directive('petDir',function() {
   return {
         restrict: 'E',
+        scope:  {
+          pets: '='
+        },
         templateUrl: "../views/pet-tmpl.html",
       };
     });
